dea72fc0d3fadfddda93129021da362fad9725e5
EU\CarreraP <patricia.carrera.93@gmail.com>
2018-07-24 12:30:27 +0200

    fragment refactor

--- Marvellous/app/src/main/java/com/example/carrerap/marvellous/view/CharactersListFragment.kt
+++ Marvellous/app/src/main/java/com/example/carrerap/marvellous/view/CharactersListFragment.kt
@@ -1,48 +1,50 @@
-package com.example.carrerap.marvellous
+package com.example.carrerap.marvellous.view
 
-import android.content.Intent
-import kotlinx.android.synthetic.main.activity_main.*
-import android.support.v7.app.AppCompatActivity
+import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.Toast
-import com.example.carrerap.marvellous.R.layout.activity_character_info
+import com.example.carrerap.marvellous.MarvelService
+import com.example.carrerap.marvellous.R
 import com.example.carrerap.marvellous.adapters.CharactersAdapter
 import com.example.carrerap.marvellous.model.ApiResponse
 import com.example.carrerap.marvellous.model.Character
+import kotlinx.android.synthetic.main.characters_list_fragment.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.net.URL
 
-
-class MainActivity : AppCompatActivity() {
+class CharactersListFragment : android.support.v4.app.Fragment() {
 
     var BASE_URL = "http://gateway.marvel.com/v1/public/"
 
     var characterList: ArrayList<Character> = ArrayList()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        loadCharacters()
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.characters_list_fragment, container, false)
+        return view
     }
 
-    private fun loadCharacters() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        loadCharacters(view)
+    }
+
+    private fun loadCharacters(view: View){
         val buildCharacters = Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
 
         val characters = buildCharacters.build()
 
-        val marvelClient = characters.create(MarvelService::class.java!!)
+        val marvelClient = characters.create(MarvelService::class.java)
         //aquí se manda el id.
         val call = marvelClient.getCharacters()
 
@@ -55,28 +57,24 @@ class MainActivity : AppCompatActivity() {
                         val photoUrl: String = apiResponse.data.results[i].thumbnail.path + "." + apiResponse.data.results[i].thumbnail.extension
                         characterList.add(Character(apiResponse.data.results[i].id, apiResponse.data.results[i].name, apiResponse.data.results[i].description, photoUrl))
                         // data class Character(val id: Int, val name: String, val info: String, val photoUrl: String)    return charactersArray
-                        rv_characters.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayout.VERTICAL, false)
+                        rv_characters.layoutManager = LinearLayoutManager(activity, LinearLayout.VERTICAL, false)
 
                         // rv_characters.adapter = CharactersAdapter(characterList, {item:Character-> View.OnClickListener(item) } )
 
-                        rv_characters.adapter = CharactersAdapter(characterList, this@MainActivity, { partItem: Character -> characterClicked(partItem) })
+                        rv_characters.adapter = CharactersAdapter(characterList, view.context, { partItem: Character -> characterClicked(partItem) })
                     }
                 }
             }
 
             override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "" + call + "" + t, Toast.LENGTH_LONG).show()
+                Toast.makeText(activity, "" + call + "" + t, Toast.LENGTH_LONG).show()
             }
         })
     }
 
     private fun characterClicked(character: Character) {
-        Toast.makeText(this, "Clicked: ${character.name}", Toast.LENGTH_LONG).show()
-        val intent = Intent(this, activity_character_info::class.java)
-        intent.putExtra("CHARACTER_ID", character.id)
-        startActivityForResult(intent, 1)
+        Toast.makeText(activity, "Clicked: ${character.name}", Toast.LENGTH_LONG).show()
+
 
     }
-
 }
-
---
+++ Marvellous/app/src/main/java/com/example/carrerap/marvellous/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.carrerap.marvellous.view
+
+
+import android.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carrerap.marvellous.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadCharacterList()
+    }
+
+    //probar privados
+    fun loadCharacterList(){
+        supportFragmentManager.beginTransaction().replace(R.id.ly_activity_main, CharactersListFragment()).addToBackStack("charactersList").commit()
+    }
+
+
+
+
+}
+
