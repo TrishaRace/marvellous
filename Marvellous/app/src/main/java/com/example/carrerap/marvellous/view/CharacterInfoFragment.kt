73d8c021f37743419a7c9787b23e9c9b2d9f363a
TrishaRace <patricia.carrera.93@gmail.com>
2018-08-30 12:55:18 +0200

    Escritura en base de datos Firebase

--- Marvellous/app/src/main/java/com/example/carrerap/marvellous/view/CharacterInfoFragment.kt
+++ Marvellous/app/src/main/java/com/example/carrerap/marvellous/view/CharacterInfoFragment.kt
@@ -1,28 +1,26 @@
 package com.example.carrerap.marvellous.view
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.carrerap.marvellous.R
 import com.example.carrerap.marvellous.model.Character
 import com.example.carrerap.marvellous.model.Comment
-import com.google.firebase.database.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.character_info_fragment.*
-import java.math.BigInteger
 import java.util.*
-
 
 class CharacterInfoFragment : android.support.v4.app.Fragment() {
 
     lateinit var character: Character
-    lateinit var mDatabase :DatabaseReference
-
+    var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    var mDatabase: DatabaseReference = database.getReference("comment")
 
     companion object {
         fun newInstance(character: Character): CharacterInfoFragment {
@@ -38,28 +36,12 @@ class CharacterInfoFragment : android.support.v4.app.Fragment() {
         super.onCreate(savedInstanceState)
         character = arguments!!.getParcelable("character")
 
-        mDatabase = FirebaseDatabase.getInstance().getReference()
-
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Get Post object and use the values to update the UI
-                val post = dataSnapshot.getValue<Comment>(Comment::class.java)
-                // ...
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
-                // ...
-            }
-        }
-        //mPostReference.addValueEventListener(postListener)
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.character_info_fragment, container, false)
 
+        
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -68,7 +50,9 @@ class CharacterInfoFragment : android.support.v4.app.Fragment() {
         loadInfoCharacter(view)
         val comicsButton = b_comics
 
-
+        b_send_comment.setOnClickListener {
+            sendComment()
+        }
         comicsButton.setOnClickListener {
             (activity as MainActivity).loadComicsList(character.comics)
         }
@@ -76,10 +60,15 @@ class CharacterInfoFragment : android.support.v4.app.Fragment() {
         val et_comment_body = et_comment_body
         val b_send_comment = b_send_comment
 
+        et_comment_body.addTextChangedListener(object : TextWatcher {
 
-        et_comment_username.addTextChangedListener(object : TextWatcher {
-
-            override fun afterTextChanged(s: Editable) {}
+            override fun afterTextChanged(s: Editable) {
+                if (et_comment_body.text.toString() != "" && et_comment_username.text.toString() != "") {
+                    b_send_comment.visibility = View.VISIBLE
+                } else {
+                    b_send_comment.visibility = View.GONE
+                }
+            }
 
             override fun beforeTextChanged(s: CharSequence, start: Int,
                                            count: Int, after: Int) {
@@ -87,14 +76,29 @@ class CharacterInfoFragment : android.support.v4.app.Fragment() {
 
             override fun onTextChanged(s: CharSequence, start: Int,
                                        before: Int, count: Int) {
-                if (et_comment_body.text != null && et_comment_username != null) {
+
+            }
+        })
+
+        et_comment_username.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {
+                if (et_comment_body.text.toString() != "" && et_comment_username.text.toString() != "") {
                     b_send_comment.visibility = View.VISIBLE
                 } else {
                     b_send_comment.visibility = View.GONE
                 }
             }
-        })
 
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+
+            }
+        })
 
 
     }
@@ -108,16 +112,20 @@ class CharacterInfoFragment : android.support.v4.app.Fragment() {
     }
 
     private fun sendComment() {
-        val random:Random = Random(20)
+        val random = Random()
         val name: String = et_comment_username.text.toString()
         val body: String = et_comment_body.text.toString()
-        val userId: String = BigInteger(50, random).toString()
+        val commentId = random.nextInt(999999999).toString()
         // si solo se quiere pasar la view como parametro: view.setOnClickListener { doSomething(it) }
         // si solo se quiere pasar la view como parametro: view.setOnClickListener { doSomething(it) }
 
-        val comment: Comment = Comment(name, body)
-        mDatabase.child("comments").child(userId).setValue(comment)
+        val comment = Comment(name, body)
+        mDatabase.child(commentId).setValue(comment)
+
+        et_comment_body.setText("")
+        et_comment_username.setText("")
+        b_send_comment.visibility = View.GONE
+        Toast.makeText(context, getString(R.string.comment_send_toast), Toast.LENGTH_SHORT).show()
     }
-
 
 }
